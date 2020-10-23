390ee276110c25f2a99d0e007b8115309cc646c2
Branson Smith <bransonsmith22@gmail.com>
2020-10-23 00:36:21 -0500

    add test sql

--- routes/projects.js
+++ routes/projects.js
@@ -122,7 +122,7 @@ router.post(inc_code_route, (req, response) => {
 router.post(inc_demo_route, (req, response) => {
     common.logReq(`POST`, inc_demo_route);
     try {
-        const sql = `UPDATE ${table_name} SET exampleclicks = exampleclicks + 1 WHERE id = ${req.params.id};`;
+        const sql = `UPDATE ${table_name} SET exampleclicks = 1 WHERE id = ${req.params.id};`;
         db.executeSql(sql, 'Increment Demo Clicks on Project').then(sqlResponse => {
             common.logResponse(inc_demo_route, sqlResponse);
             response.status(200).send(dbResponse.result);
