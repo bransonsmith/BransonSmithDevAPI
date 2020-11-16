2a111f8cdce0bd2c56cd3c28f7340d699ee1edef
Branson Smith <bransonsmith22@gmail.com>
2020-11-16 14:33:25 -0600

    unconst keys

--- _services/budget/label-service.js
+++ _services/budget/label-service.js
@@ -10,7 +10,7 @@ async function getLabelByDetails(details, title=`Get label by details`) {
 
     for (let i = 0; i < labels.length; i++) {
         const label = labels[i];
-        const keys = [];
+        let keys = [];
         if (label.autokeys !== undefined && label.autokeys !== null && label.autokeys.length > 0) {
             keys = label.autokeys.split(',');
             for (let j = 0; j < keys.length; j++) {
