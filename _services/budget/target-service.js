318b6534214ae02d776c2e8c3799fc0cc9a686e1
Branson Smith <bransonsmith22@gmail.com>
2021-01-28 12:34:53 -0600

    remove dep function

--- _services/budget/target-service.js
+++ _services/budget/target-service.js
@@ -13,14 +13,4 @@ async function getTargetsForMonth(incomeids, title='Get targets for month') {
     return { status: 200, result: rows }
 }
 
-// async function getTargetsForIncomes(incomeids, title='Get targets for incomes') {
-
-//     const sql = `SELECT ${sqlStrings.getDtoFieldString(table_name)} FROM ${table_name} WHERE incomeid IN ('${incomeids.join("', '")}');`;
-//     const dbResponse = await db.executeSql(sql, title);
-//     if (dbResponse.status !== 200) { return dbResponse; }
-    
-//     const rows = dbResponse.result.rows;
-//     return { status: 200, result: rows }
-// }
-
 module.exports.getTargetsForMonth = getTargetsForMonth;
