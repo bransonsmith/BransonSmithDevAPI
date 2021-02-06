019cb89de5c32459fbfa21dcbe0602ae789f7cbd
Branson Smith <bransonsmith22@gmail.com>
2021-02-05 23:27:18 -0600

    Point to hole service

--- _controllers/discgolf/hole-controller.js
+++ _controllers/discgolf/hole-controller.js
@@ -3,7 +3,7 @@ let router = express.Router();
 const common = require("../../common");
 const logging = require("../../logging");
 const baseController = require("../base-controller");
-const courseService = require("../../_services/discgolf/course-service");
+const holeService = require("../../_services/discgolf/hole-service");
 
 const base_route = common.base_route;
 const table_name = 'discgolfholes'
@@ -11,7 +11,7 @@ const table_name = 'discgolfholes'
 router.get(`${base_route}/${table_name}/course/:courseid`, (req, response) => {
     logging.logRequest(req);
     const courseid = req.params.courseid;
-    courseService.getHolesForCourse(courseid, req.body).then(serviceResponse => {
+    holeService.getHolesForCourse(courseid, req.body).then(serviceResponse => {
         baseController.handleServiceResponse(serviceResponse, req, response);
     });
 });
--- _services/discgolf/hole-service.js
+++ _services/discgolf/hole-service.js
@@ -3,7 +3,7 @@ const baseService = require("../base-service");
 const table_name = 'discgolfholes';
 
 async function getHolesForCourse(courseId, title='Get holes for disc golf course') {
-
+    
     const holesResponse = await baseService.getAll(table_name);
     if (holesResponse.status !== 200) { return holesResponse; }
     const holes = holesResponse.result;
