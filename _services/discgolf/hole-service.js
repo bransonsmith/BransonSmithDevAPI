be8ab5e4532ae2856ce61591afdbd07d90580b7b
Branson Smith <bransonsmith22@gmail.com>
2021-02-05 23:08:52 -0600

    Add holes for course call

---
+++ _services/discgolf/hole-service.js
@@ -0,0 +1,14 @@
+const baseService = require("../base-service");
+
+const table_name = 'discgolfholes';
+
+async function getHolesForCourse(courseId, title='Get holes for disc golf course') {
+
+    const holesResponse = await baseService.getAll(table_name);
+    if (holesResponse.status !== 200) { return holesResponse; }
+    const holes = holesResponse.result;
+
+    return { status: 200, result: holes.filter(h => h.courseid === courseid) };
+}
+
+module.exports.getHolesForCourse = getHolesForCourse;
--- _services/discgolf/player-holes-service.js
+++ _services/discgolf/player-holes-service.js
@@ -2,7 +2,7 @@ const baseService = require("../base-service");
 
 const table_name = 'discgolfplayerholes';
 
-async function getPlayerHolesForPlayerRound(playerroundid, title='Get player rounds for disc golf round') {
+async function getPlayerHolesForPlayerRound(playerroundid, title='Get player holes for disc golf round') {
 
     const playerHolesResponse = await baseService.getAll(table_name);
     if (playerHolesResponse.status !== 200) { return playerHolesResponse; }
