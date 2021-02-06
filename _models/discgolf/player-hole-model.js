b42d73e11fb859f32cd1b465588ba92c577eb1f3
Branson Smith <bransonsmith22@gmail.com>
2021-02-06 17:41:51 -0600

    Set derfault hole values

--- _models/discgolf/player-hole-model.js
+++ _models/discgolf/player-hole-model.js
@@ -2,20 +2,17 @@ const Joi = require('joi');
 
 const table_name = 'discgolfplayerholes';
 const all_fields = [
-    { name: 'id',         type: 'varchar(255)', attributes: 'NOT NULL PRIMARY KEY', mustHaveExistingObject: false, table: '',               onDto: true , createField: false, updateField: false, optional: false },
-    { name: 'playerroundid',   type: 'varchar(255)', attributes: 'NOT NULL',             mustHaveExistingObject: true,  table: 'discgolfplayers', onDto: true, createField: true , updateField: true , optional: false },
-    { name: 'holeid',     type: 'varchar(255)', attributes: 'NOT NULL',             mustHaveExistingObject: true,  table: 'discgolfholes',   onDto: true, createField: true , updateField: true , optional: false },
-    { name: 'score',      type: 'int',          attributes: 'NOT NULL',             mustHaveExistingObject: false, table: '',               onDto: true , createField: true , updateField: true , optional: true  },
-    { name: 'dots',       type: 'int',          attributes: 'NOT NULL',             mustHaveExistingObject: false, table: '',               onDto: true , createField: true , updateField: true , optional: true  },
-    { name: 'shots',      type: 'varchar(255)', attributes: 'NOT NULL',             mustHaveExistingObject: false, table: '',               onDto: true , createField: true , updateField: true , optional: true  },
+    { name: 'id',         type: 'varchar(255)', attributes: 'NOT NULL PRIMARY KEY', mustHaveExistingObject: false, table: '',                     onDto: true,  createField: false,  updateField: false, optional: false },
+    { name: 'playerroundid',   type: 'varchar(255)', attributes: 'NOT NULL',             mustHaveExistingObject: true,  table: 'discgolfplayers', onDto: true,  createField: true ,  updateField: true , optional: false },
+    { name: 'holeid',     type: 'varchar(255)', attributes: 'NOT NULL',             mustHaveExistingObject: true,  table: 'discgolfholes',        onDto: true,  createField: true ,  updateField: true , optional: false },
+    { name: 'score',      type: 'int',          attributes: 'NOT NULL',             mustHaveExistingObject: false, table: '',                     onDto: true , createField: false , updateField: true , optional: true  },
+    { name: 'dots',       type: 'int',          attributes: 'NOT NULL',             mustHaveExistingObject: false, table: '',                     onDto: true , createField: false , updateField: true , optional: true  },
+    { name: 'shots',      type: 'varchar(255)', attributes: 'NOT NULL',             mustHaveExistingObject: false, table: '',                     onDto: true , createField: false , updateField: true , optional: true  },
 ];
 
 const createSchema = Joi.object({
     playerroundid: Joi.string().required().max(255),
     holeid: Joi.string().required().max(255),
-    score: Joi.number(),
-    dots: Joi.number(),
-    shots: Joi.string().max(255),
 });
 const updateSchema = Joi.object({
     playerroundid: Joi.string().required().max(255),
@@ -26,13 +23,18 @@ const updateSchema = Joi.object({
 });
 
 function getCreateValues(body, newId) {
+    
+    const score = 0;
+    const dots = 0;
+    const shots = '';
+
     return [
         `'${newId}'`,
         `'${body.playerroundid}'`,
         `'${body.holeid}'`,
-        `${body.score}`,
-        `${body.dots}`,
-        `'${body.shots}'`,
+        `${score}`,
+        `${dots}`,
+        `'${shots}'`,
     ];
 }
 
