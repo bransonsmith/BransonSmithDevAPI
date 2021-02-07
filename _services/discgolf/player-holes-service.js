15b937f2a322ceeb4e1c638095a9887c9d9ac2d6
Branson Smith <bransonsmith22@gmail.com>
2021-02-07 17:25:57 -0600

    Add hole to filled out player hole

--- _services/discgolf/player-holes-service.js
+++ _services/discgolf/player-holes-service.js
@@ -7,8 +7,27 @@ async function getPlayerHolesForPlayerRound(playerroundid, title='Get player hol
     const playerHolesResponse = await baseService.getAll(table_name);
     if (playerHolesResponse.status !== 200) { return playerHolesResponse; }
     const playerHoles = playerHolesResponse.result;
+    const filteredHoles = playerHoles.filter(h => h.playerroundid === playerroundid);
 
-    return { status: 200, result: playerHoles.filter(h => h.playerroundid === playerroundid) };
+    const filledOutPlayerHoles = [];
+    for (let i = 0; i < filteredHoles.length; i++) {
+        const h = filteredHoles[i];
+        const holeResponse = await baseService.getOne('discgolfplayerholes', h.holeid);
+        if (holeResponse.status !== 200) { return holeResponse; }
+        const hole = holeResponse.result;
+
+        filledOutPlayerHoles.push({
+            id: h.id,
+            playerroundid: playerroundid,
+            holeid: hole.id,
+            hole: hole,
+            score: h.score,
+            dots: h.dots,
+            shots: h.shots
+        });
+    }
+
+    return { status: 200, result: filledOutPlayerHoles };
 }
 
 module.exports.getPlayerHolesForPlayerRound = getPlayerHolesForPlayerRound;
