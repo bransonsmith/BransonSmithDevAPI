b4382382b8dd482b2e8694c831ab2f8e5f18a450
Branson Smith <bransonsmith22@gmail.com>
2021-02-09 15:12:38 -0600

    Switch to nodemailer client

--- _controllers/email-controller.js
+++ _controllers/email-controller.js
@@ -1,25 +1,46 @@
 const express = require("express");
-const send = require('gmail-send')({
-    user: 'bransonsmithdev@gmail.com',
-    pass: 'bsdev316',
-    to:   'bransonsmith22@gmail.com',
-    subject: 'Message From Website Contact Page',
-  });
+var nodemailer = require('nodemailer');
+// const send = require('gmail-send')({
+//     user: 'bransonsmithdev@gmail.com',
+//     pass: 'bsdev316',
+//     to:   'bransonsmith22@gmail.com',
+//     subject: 'Message From Website Contact Page',
+//   });
 let router = express.Router();
 
 router.post('/api/emails/send', (req, response) => {
-    console.log('POST /api/emails/send');
-    console.log(req.body);
-
+    
     var message = 'From ' + req.body.fromEmail + ' - ' + req.body.message;
 
-    send({
-        text:    message,  
-      }, (error, result, fullResult) => {
-        if (error) console.error(error);
-        console.log(result);
-    })
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'bransonsmithdev@gmail.com',
+            pass: 'bsdev316'
+        }
+    });
     
+    var mailOptions = {
+        from: 'bransonsmithdev@gmail.com',
+        to: 'bransonsmith22@gmail.com',
+        subject: 'Message From Website Contact Page',
+        text: message
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
 });
 
 module.exports = router;
+
+
+
+
+
+
+
